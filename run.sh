6a7b76e3b2ae675e232683dcfd538bac0e3fac87
Sebastiano M. Cossu <sebastianocossu@hotmail.com>
2017-12-05 15:07:35 +0100

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,5 @@
+./bin/raytrace -r 720 -s 3 -o out/basic.png in/basic_pointlight/basic_pointlight.obj
+./bin/raytrace -r 720 -s 3 -o out/simple.png in/simple_pointlight/simple_pointlight.obj
+./bin/raytrace -r 720 -s 3 -o out/lines.png in/lines_pointlight/lines_pointlight.obj
+./bin/raytrace -r 720 -s 3 -o out/instance.png in/instance10000_pointlight/instance10000_pointlight.obj
+./bin/raytrace -r 720 -s 3 -o out/refl.png in/refl_pointlight/refl_pointlight.obj
